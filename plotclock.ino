2f1d1be73403de523bb948c441ea3fa9f1713e2b
Alex Ozerin <m12sl@Thunderclap.local>
2015-03-28 12:14:55 +0300

    fix date setting

--- plotclock.ino
+++ plotclock.ino
@@ -122,7 +122,10 @@ void setup()
 //   // Set current time only the first to values, hh,mm are needed
 //   setTime(19,38,0,0,0,0);
 // #endif
-  system("date -s \"28 MAR 2015 12:30:00\"");
+
+  system("date 1231024913");
+  //MMDDhhmmYYYY.ss format:)
+
   drawTo(75.2, 47);
   lift(0);
   pinMode(SERVOPINLIFT, OUTPUT);
